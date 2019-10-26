2b3c5ae0e76daebb7e01baf2a93639dc10815cf0
MBtsky <9250499222@mail.ru>
2019-10-26 12:35:04 +0300

    unnecessary tests removed

--- staticLib/tests.cpp
+++ staticLib/tests.cpp
@@ -37,20 +37,6 @@ TEST_CASE("Four Points №2", "Single Process algorithm") {
     REQUIRE( computeLengthSingleThreaded(points, 2) == 255 * sqrt(2));
     delete(points);
 }
-//TEST_CASE("Many points №2", "Single Process algorithm") {
-//
-//    Points* points = new Points(6);
-//
-//    points[0] = 0b10000000100000000000000000000000;
-//    points[1] = 0b1000000010000000111111101111111;
-//    points[2] = 0b10000000100000000000000000000000;
-//    points[3] = 0b1000000010000000111111101111111;
-//    points[4] = 0b10000000100000000000000000000000;
-//    points[5] = 0b1000000010000000111111101111111;
-//
-//    REQUIRE( computeLengthSingleThreaded(points, 6) == 255 * sqrt(2) * 3 + 128 * sqrt(2) * 4);
-//    delete(points);
-//}
 TEST_CASE("Two Points_multi", "Multi Process algorithm") {
 
     Points* points = new Points(1);
@@ -77,17 +63,3 @@ TEST_CASE("Four Points №2_multi", "Multi Process algorithm") {
     REQUIRE( static_run(points, 2) == 255 * sqrt(2));
     delete(points);
 }
-//TEST_CASE("Many points №2_multi", "Multi Process algorithm") {
-//
-//    Points* points = new Points(6);
-//
-//    points[0] = 0b10000000100000000000000000000000;
-//    points[1] = 0b1000000010000000111111101111111;
-//    points[2] = 0b10000000100000000000000000000000;
-//    points[3] = 0b1000000010000000111111101111111;
-//    points[4] = 0b10000000100000000000000000000000;
-//    points[5] = 0b1000000010000000111111101111111;
-//
-//    REQUIRE( static_run(points, 6) == 255 * sqrt(2) * 3);
-//    delete(points);
-//}
